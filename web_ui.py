0ee9a93f57de61f9724412c306da4ec776808b6c
ljwen17 <ljwen17@grgbanking.com>
2024-02-22 11:16:37 +0800

    优化知识库

--- web_ui.py
+++ web_ui.py
@@ -1,6 +1,7 @@
 import json
 import os
 import re
+import shutil
 from typing import List, Dict
 
 import gradio as gr
@@ -29,6 +30,51 @@ def select_llm(use_local_llm):
 
 def clear_historys():
     yield None
+
+
+def get_knowledge_table_list(knowledge_txt):
+    knowledge_table = list([])
+    with open(os.path.join("knowledge", knowledge_txt), 'r') as file:
+        for line in file:
+            knowledge_table.append([line])
+    return knowledge_table
+
+
+def get_knowledge_list():
+    lst_default = []
+    kb_root_path = "knowledge"
+    lst = os.listdir(kb_root_path)
+    if not lst:
+        return lst_default
+    lst.sort()
+    return lst_default + lst
+
+
+def upload_knowledge(files):
+    file_paths = [file.name for file in files]
+    kb_root_path = "knowledge"
+    for file_path in file_paths:
+        # 复制文件到kb_root_path目录
+        shutil.copy(file_path, kb_root_path)
+    return gr.update(choices=get_knowledge_list())
+
+
+PAGE_SIZE = 10
+
+
+def prev_page(page_index: int) -> int:
+    return page_index - 1 if page_index > 1 else page_index
+
+
+def next_page(page_index: int, total_num: int) -> int:
+    return page_index + 1 if page_index * PAGE_SIZE < total_num else page_index
+
+
+def get_preview(kb, page):
+    if kb is None or len(kb) == 0:
+        return 0, [], gr.update(visible=False)
+    knowledges = get_knowledge_table_list(kb)
+    return len(knowledges), knowledges[PAGE_SIZE * (page - 1): PAGE_SIZE * page], gr.update(visible=True)
 
 
 def get_pre_answer(query, history):
@@ -93,7 +139,25 @@ block_css = """.importantButton {
 .importantButton:hover {
     background: linear-gradient(45deg, #ff00e0,#8500ff, #6e00ff) !important;
     border: none !important;
-}"""
+}
+.modal-box {
+  position: fixed !important;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%); /* center horizontally */
+  max-width: 1000px;
+  max-height: 750px;
+  overflow-y: auto;
+  background-color: var(--input-background-fill);
+  flex-wrap: nowrap !important;
+  border: 2px solid black !important;
+  z-index: 1000;
+  padding: 10px;
+}
+.dark .modal-box {
+  border: 2px solid white !important;
+}
+"""
 
 default_theme_args = dict(
     font=["Source Sans Pro", 'ui-sans-serif', 'system-ui', 'sans-serif'],
@@ -105,12 +169,16 @@ def start_ui():
     with gr.Blocks(css=block_css, theme=gr.themes.Default(**default_theme_args)) as demo:
         with gr.Tab("Inter Agent"):
             with gr.Row():
-                with gr.Column(scale=15):
+                with gr.Column(scale=20):
                     # 聊天框模块
                     chatbot = gr.Chatbot([], height=1000, label="对话",
                                          elem_id="chat-box")
-
-                with gr.Column(scale=5):
+                    with gr.Row():
+                        with gr.Column(scale=18):
+                            query = gr.Textbox(label="问题", placeholder="请输入提问内容，按回车进行提交")
+                        with gr.Column(scale=2):
+                            clear = gr.Button("清空历史")
+                with gr.Column(scale=8):
                     with gr.Accordion("Agent配置"):
                         with gr.Accordion("模型配置"):
                             use_local_llm = gr.Radio(["是", "否"], label="使用本地模型", value="是")
@@ -120,22 +188,56 @@ def start_ui():
                                                         value="127.0.0.1")
                             local_llm_port = gr.Number(label="本地模型端口", value=8888, precision=0)
                             openai_api_key = gr.Textbox(label="OPENAI_API_KEY", visible=False)
-                        lang = gr.Radio(["zh", "en"], label="语言", value="zh")
-                        max_iter_num = gr.Number(label="最大思考次数", value=5, precision=0)
-                        tools = gr.CheckboxGroup(["web_search", "browse_website",
-                                                  "get_weather_info", "get_calendar_info",
-                                                  "time_delta", "get_solar_terms_info",
-                                                  "image_gen", "code_interpreter", "math_calculater",
-                                                  "bilibili_crawler"],
-                                                 label="工具", info="默认全部")
+                        with gr.Accordion("知识库"):
+                            kbt = gr.Dropdown(choices=get_knowledge_list(), label="知识库文件")
+
+                            with gr.Row():
+                                upload_button = gr.UploadButton("上传", file_types=["text"],
+                                                                file_count="multiple")
+                                data_preview_btn = gr.Button("预览")
+                                upload_button.upload(upload_knowledge, upload_button, kbt)
+
+                            with gr.Column(visible=False, elem_classes="modal-box") as preview_box:
+                                kdf = gr.List(
+                                    headers=["知识"],
+                                    datatype=["str"],
+                                    type="array"
+                                )
+                                with gr.Row():
+                                    preview_count = gr.Number(label="总数量", value=0, interactive=False, precision=0)
+                                    page_index = gr.Number(label="页数", value=1, minimum=1, interactive=False,
+                                                           precision=0)
+                                with gr.Row():
+                                    prev_btn = gr.Button("上一页")
+                                    next_btn = gr.Button("下一页")
+                                    close_btn = gr.Button("关闭")
+
+                            data_preview_btn.click(
+                                get_preview,
+                                [kbt, page_index],
+                                [preview_count, kdf, preview_box]
+                            )
+                            prev_btn.click(prev_page, [page_index], [page_index]).then(
+                                get_preview, [kbt, page_index], [preview_count, kdf, preview_box]
+                            )
+                            next_btn.click(next_page, [page_index, preview_count], [page_index]).then(
+                                get_preview, [kbt, page_index], [preview_count, kdf, preview_box]
+                            )
+                            close_btn.click(lambda: gr.update(visible=False), outputs=[preview_box])
+                        with gr.Accordion("工具"):
+                            tools = gr.CheckboxGroup(["web_search", "browse_website",
+                                                      "get_weather_info", "get_calendar_info",
+                                                      "time_delta", "get_solar_terms_info",
+                                                      "image_gen", "code_interpreter", "math_calculater",
+                                                      "bilibili_crawler"],
+                                                     label="工具集", info="默认全部")
+                        with gr.Accordion("其他", open=False):
+                            with gr.Row():
+                                max_iter_num = gr.Number(label="最大思考次数", value=3, precision=0)
+                                lang = gr.Radio(["zh", "en"], label="语言", value="zh")
+
                         use_local_llm.change(fn=select_llm, inputs=[use_local_llm],
                                              outputs=[llm_name, local_llm_host, local_llm_port, openai_api_key])
-
-            with gr.Row():
-                with gr.Column(scale=15):
-                    query = gr.Textbox(label="问题", placeholder="请输入提问内容，按回车进行提交")
-                with gr.Column(scale=5):
-                    clear = gr.Button("清空历史")
 
                 query.submit(fn=get_pre_answer,
                              inputs=[query, chatbot],
